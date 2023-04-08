1546e2fb153effd725e01df882ac28ec02dc3513
OBro1961 <oamcf26@gmail.com>
2023-04-08 11:37:04 -0500

    Fixes #72.

--- src/main/java/obro1961/chatpatches/mixin/MinecraftClientMixin.java
+++ src/main/java/obro1961/chatpatches/mixin/MinecraftClientMixin.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import obro1961.chatpatches.ChatPatches;
 import obro1961.chatpatches.chatlog.ChatLog;
 import obro1961.chatpatches.gui.ChatSearchScreen;
@@ -43,12 +42,12 @@ public abstract class MinecraftClientMixin {
 
     /** Replaces the vanilla chat screen with the search-enabled one */
     @WrapOperation(method = "openChatScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 1))
-    private void cps$overrideChatScreen(MinecraftClient instance, Screen vanillaChatScreen, Operation<Void> setScreenMethod) {
-        if(ChatPatches.config.chatSearchScreen) {
-            TextFieldWidget chatField = ((TextFieldWidget) vanillaChatScreen.getFocused());
-            setScreenMethod.call(instance, new ChatSearchScreen( chatField != null ? chatField.getText() : "" ));
-        } else {
-            setScreenMethod.call(instance, vanillaChatScreen);
-        }
+    private void cps$overrideChatScreen(MinecraftClient mc, Screen vanillaChatScreen, Operation<Void> setScreenMethod, String initialText) {
+
+        if(ChatPatches.config.chatSearchScreen)
+            setScreenMethod.call(mc, new ChatSearchScreen(initialText));
+
+        else
+            setScreenMethod.call(mc, vanillaChatScreen);
     }
 }
