d5a9449b7d5de5687edd3b84cf56e02834dc52f5
OBro1961 <oamcf26@gmail.com>
2025-07-10 21:04:53 -0500

    Try and fail to copy built versions to root `build/libs/`

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -8,12 +8,32 @@ stonecutter active "1.21.7-fabric"
 fun registerLoaderBuildTask(loader: String) {
     // not sure why Kotlin deprecated capitalize() ...
     tasks.register("build${loader.replaceFirstChar { it.uppercase() } }") {
-        // todo: copy to rootProject.layout.buildDirectory.file("libs")
         dependsOn(
             stonecutter.tasks.named("build") {
                 metadata.project.endsWith(loader)
             }
         )
+
+        // todo: copy shit doesn't work bc the copy block returns a WorkResult and not a Task -_-
+        /*copy {
+            val versions = (findProperty("targets") as String?)?.split(",") ?: emptyList()
+            if(versions.isEmpty()) {
+                println("no version targets..")
+                return@copy
+            }
+
+            var libs = rootProject.layout.buildDirectory.file("libs/").get().asFile
+            var _temp_ = libs.createNewFile() // ensure the directory exists
+            println("created libs '${libs.path}' : $_temp_")
+
+            from(versions.map {
+                val vLibs = rootProject.file("versions/$it-$loader/build/libs/") // maps each version to its build/libs directory
+                println("created vLibs '${vLibs.path}' : ${  vLibs.createNewFile()  }")
+
+                vLibs
+            })
+            into(libs)
+        }*/
     }
 }
 
