e538175d3b207dcfde5b882965bf84e38a798353
OBro1961 <oamcf26@gmail.com>
2025-07-13 21:54:22 -0500

    Make `build<loader>` tasks copy libs
    + ty to this link for the inspo! https://github.com/murderspagurder/mod-template-java/blob/main/build.fabric.gradle.kts#L64C1-L69C6

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -6,41 +6,39 @@ stonecutter active "1.21.7-fabric"
 
 // each build task builds all of its loader and copies them to build/libs/
 fun registerLoaderBuildTask(loader: String) {
-    // not sure why Kotlin deprecated capitalize() ...
-    tasks.register("build${loader.replaceFirstChar { it.uppercase() } }") {
+    // registers as a Copy task to allow easy copying
+    tasks.register<Copy>("build${loader.replaceFirstChar { it.uppercase() } }") { // not sure why Kotlin deprecated capitalize() ...
+        group = "build"
+
+        // builds the loader's jars (lazily)
         dependsOn(
             stonecutter.tasks.named("build") {
                 metadata.project.endsWith(loader)
             }
         )
 
-        // todo: copy shit doesn't work bc the copy block returns a WorkResult and not a Task -_-
-        /*copy {
-            val versions = (findProperty("targets") as String?)?.split(",") ?: emptyList()
-            if(versions.isEmpty()) {
-                println("no version targets..")
-                return@copy
-            }
-
+        // copies them to build/libs/
+        val versions = stonecutter.versions.filter { it.project.endsWith(loader) }.map { it.version } // gets this loader's versions
+        if(versions.isNotEmpty()) {
             var libs = rootProject.layout.buildDirectory.file("libs/").get().asFile
-            var _temp_ = libs.createNewFile() // ensure the directory exists
-            println("created libs '${libs.path}' : $_temp_")
+            libs.createNewFile() // ensure the directory exists
 
             from(versions.map {
                 val vLibs = rootProject.file("versions/$it-$loader/build/libs/") // maps each version to its build/libs directory
-                println("created vLibs '${vLibs.path}' : ${  vLibs.createNewFile()  }")
-
+                vLibs.createNewFile()
                 vLibs
             })
             into(libs)
-        }*/
+        } else {
+            println("Warning: No versions specified for $loader; ensure `targets` is correctly populated")
+        }
     }
 }
 
 registerLoaderBuildTask("fabric")
 registerLoaderBuildTask("neo")
-//registerLoaderBuildTask("forge") // todo
+registerLoaderBuildTask("forge")
 
 tasks.register("buildAll") {
-    dependsOn("buildFabric", "buildNeo"/*, "buildForge"*/)
+    dependsOn("buildFabric", "buildNeo", "buildForge")
 }
