f42091c65e11d6b610602ca31953e8318f2d8d8a
OBro1961 <oamcf26@gmail.com>
2025-07-17 23:05:37 -0500

    Clean up stonecutter.gradle

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -20,7 +20,7 @@ fun registerLoaderBuildTask(loader: String) {
         // copies them to build/libs/
         val versions = stonecutter.versions.filter { it.project.endsWith(loader) }.map { it.version } // gets this loader's versions
         if(versions.isNotEmpty()) {
-            var libs = rootProject.layout.buildDirectory.dir("libs").get().asFile
+            val libs = rootProject.layout.buildDirectory.dir("libs").get().asFile
             // ensure the directory exists
             if(!libs.exists()) {
                 libs.parentFile.mkdirs() // avoid IOExceptions
@@ -30,8 +30,6 @@ fun registerLoaderBuildTask(loader: String) {
             from(versions.map {
                 val vLibs = rootProject.layout.projectDirectory.dir("versions/$it-$loader/build/libs/").asFile // maps each version to its build/libs directory
                 if(!vLibs.exists()) {
-                    println("$it-libs doesn't exist; parent exists? ${vLibs.parentFile.exists()}")
-                    if(vLibs.parentFile.exists()) println("file=${vLibs.parentFile.isFile} dir=${vLibs.parentFile.isDirectory}")
                     vLibs.parentFile.mkdirs()
                     vLibs.mkdir()
                 }
