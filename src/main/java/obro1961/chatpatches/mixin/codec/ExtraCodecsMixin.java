b6750700eb1955ad2b3ee27095fb39b7ebacbe77
OBro1961 <oamcf26@gmail.com>
2025-07-22 00:40:20 -0500

    Try workaround #246

---
+++ src/main/java/obro1961/chatpatches/mixin/codec/ExtraCodecsMixin.java
@@ -0,0 +1,33 @@
+package obro1961.chatpatches.mixin.codec;
+
+import com.mojang.serialization.DataResult;
+import net.minecraft.util.ExtraCodecs;
+import obro1961.chatpatches.ChatLog;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import java.util.function.Function;
+
+@Mixin(ExtraCodecs.class)
+public class ExtraCodecsMixin {
+	//? if >=1.21.5 {
+	/**
+	 * Allows all chat strings to be serialized while restoring chat logs. Fixes
+	 * <a href="https://github.com/mrbuilder1961/ChatPatches/issues/246">#246</a>,
+	 * which seems to only affect 1.21.5+.
+	 */
+	@ModifyArg(
+		method = "<clinit>",
+		slice = @Slice(
+			to = @At(value = "FIELD:LAST", target = "Lnet/minecraft/util/ExtraCodecs;CHAT_STRING:Lcom/mojang/serialization/Codec;", opcode = Opcodes.PUTSTATIC)
+		),
+		at = @At(value = "INVOKE:FIRST", target = "Lcom/mojang/serialization/Codec;validate(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;")
+	)
+	private static Function<String, DataResult<String>> allowEverythingWhileRestoring(Function<String, DataResult<String>> checker) {
+		return ChatLog.isRestoring() ? DataResult::success : checker;
+	}
+	//? }
+}
