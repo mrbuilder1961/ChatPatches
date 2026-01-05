ddf15dbf00af8dfff510dbeccb0d2aa10d13ef5e
OBro1961 <oamcf26@gmail.com>
2026-01-04 23:51:25 -0600

    Clean `ChatHeadsIntegration`
    + added javadoc explaining the currently used method
    - removed the old reflection-calling method, its initializer, and related fields

--- src/main/java/obro1961/chatpatches/integration/ChatHeadsIntegration.java
+++ src/main/java/obro1961/chatpatches/integration/ChatHeadsIntegration.java
@@ -1,16 +1,15 @@
 //? if >=1.21.9 {
 package obro1961.chatpatches.integration;
 
-import com.mojang.authlib.GameProfile;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.GuiMessageTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.contents.ObjectContents;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.network.chat.contents.objects.PlayerSprite;
-import obro1961.chatpatches.util.ChatUtil;
+import obro1961.chatpatches.mixin.gui.ChatComponentMixin;
+import org.spongepowered.asm.mixin.injection.points.MethodHead;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -20,13 +19,6 @@ import java.util.Optional;
 import static obro1961.chatpatches.ChatPatches.LOGGER;
 
 public class ChatHeadsIntegration {
-	private static final String CLASS_COMPONENT_PROCESSOR = "dzwdz.chat_heads.ComponentProcessor";
-	private static final String METHOD_CREATE_CHAT_HEAD_COMPONENT = "createChatHeadComponent";
-	/**
-	 * (MutableComponent) ComponentProcessor.createChatHeadComponent(PlayerInfo)
-	 */
-	private static final Method createChatHeadComponent;
-
 	private static final String CLASS_CHAT_HEADS = "dzwdz.chat_heads.ChatHeads";
 	private static final String FIELD_CONFIG = "CONFIG";
 	/**
@@ -44,21 +36,19 @@ public class ChatHeadsIntegration {
 	static {
 		enabled = true;
 
-		Method m1 = null, m2 = null; Field f1 = null; // java sometimes i really hate you.
+		Method m1 = null; Field f1 = null; // java sometimes i really hate you.
 		if(installed()) {
 			try {
-				m1 = Class.forName(CLASS_COMPONENT_PROCESSOR).getDeclaredMethod(METHOD_CREATE_CHAT_HEAD_COMPONENT, PlayerInfo.class);
 				f1 = Class.forName(CLASS_CHAT_HEADS).getDeclaredField(FIELD_CONFIG);
-				m2 = Class.forName(CLASS_CHAT_HEADS_CONFIG).getDeclaredMethod(METHOD_RENDER_POSITION);
+				m1 = Class.forName(CLASS_CHAT_HEADS_CONFIG).getDeclaredMethod(METHOD_RENDER_POSITION);
 			} catch(ClassNotFoundException | NoSuchMethodException | NoSuchFieldException e) {
 				LOGGER.error("Failed to reflectively access Chat Heads' data, disabling further interoperability:", e);
 				enabled = false;
 			}
 		}
 
-		createChatHeadComponent = m1;
 		CONFIG = f1;
-		renderPosition = m2;
+		renderPosition = m1;
 	}
 
 	private static boolean enabled;
@@ -86,56 +76,47 @@ public class ChatHeadsIntegration {
 		return false; // assumes not installed or broken - aka do nothing
 	}
 
+	/**
+	 * Custom method created by one of the authors of Chat Heads!
+	 * @author <a href="https://github.com/Fourmisain">Fourmisain</a>!
+	 *
+	 * @return An {@link Optional} enclosing the component containing the head
+	 * object, or an empty Optional if
+	 *
+	 * @param message The chat message to look for a head component aka
+	 * {@link PlayerSprite} object in. This message must be unmodified by
+	 * Chat Patches but modified by Chat Heads: so at the time of writing,
+	 * after {@link MethodHead} of addMessage(...) but before the
+	 * {@link ChatComponentMixin#modifyMessage(Component, GuiMessageTag)}
+	 * injector is called.
+	 *
+	 * @see <a href="https://github.com/dzwdz/chat_heads/issues/83">ChatHeads#83</a>
+	 * and <a href="https://github.com/mrbuilder1961/ChatPatches/issues/285">#285</a>.
+	 */
 	public static Optional<MutableComponent> extractHeadComponent(Component message) {
-			if(message.getContents() instanceof ObjectContents(PlayerSprite ignored)) {
-				return Optional.of((MutableComponent) message);
-			}
+		if(message.getContents() instanceof ObjectContents(PlayerSprite ignored)) {
+			return Optional.of((MutableComponent) message);
+		}
 
-			if(message.getContents() instanceof TranslatableContents translatable) {
-				for(var arg : translatable.getArgs()) {
-					if(arg instanceof Component c) {
-						var head = extractHeadComponent(c);
-						if(head.isPresent()) {
-							return head;
-						}
+		if(message.getContents() instanceof TranslatableContents translatable) {
+			for(var arg : translatable.getArgs()) {
+				if(arg instanceof Component c) {
+					var head = extractHeadComponent(c);
+					if(head.isPresent()) {
+						return head;
 					}
 				}
 			}
+		}
 
-			for(var sibling : message.getSiblings()) {
-				var head = extractHeadComponent(sibling);
-				if(head.isPresent()) {
-					return head;
-				}
-			}
-
-			return Optional.empty();
-	}
-
-	// not used atm - hopefully the above method is robust enough and works well for a while
-	public static MutableComponent createChatHeadComponent(GameProfile player) {
-		MutableComponent result = Component.empty();
-
-		if(enabled && usingBeforeName() && !ChatUtil.NIL_SENDER.equals(player)) {
-			try {
-				PlayerInfo playerInfo = new PlayerInfo(player, false);
-
-				var packetListener = Minecraft.getInstance().getConnection();
-				// returns null if the id does not map to a real player
-				if(packetListener.getPlayerInfo(player.id()) instanceof PlayerInfo p) {
-					playerInfo = p;
-				} else if(packetListener.getPlayerInfo(player.name()) instanceof PlayerInfo p) {
-					playerInfo = p;
-				}
-
-				result = (MutableComponent) createChatHeadComponent.invoke(null, playerInfo);
-			} catch(IllegalAccessException | InvocationTargetException e) {
-				LOGGER.error("Failed to create a chat head component:", e);
-				result = Component.empty();
+		for(var sibling : message.getSiblings()) {
+			var head = extractHeadComponent(sibling);
+			if(head.isPresent()) {
+				return head;
 			}
 		}
 
-		return result;
+		return Optional.empty();
 	}
 }
 //?}
