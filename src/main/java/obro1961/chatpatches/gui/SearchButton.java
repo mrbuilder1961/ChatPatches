0dd40d55e423dca12b3f86b0f0905499a2d50c6e
OBro1961 <oamcf26@gmail.com>
2026-06-23 13:07:56 -0500

    Convert most swaps to replacements
    + not entirely done but mostly

--- src/main/java/obro1961/chatpatches/gui/SearchButton.java
+++ src/main/java/obro1961/chatpatches/gui/SearchButton.java
@@ -35,7 +35,7 @@ public class SearchButton extends ImageButton {
     }
 
     @Override
-    public boolean mouseClicked(/*$ mouse_event {*/MouseButtonEvent mouse, boolean bl/*$}*/) {
+    public boolean mouseClicked(MouseButtonEvent mouse, boolean bl) {
         //? if >=1.21.9 {
         double mX = mouse.x(), mY = mouse.y();
         int button = mouse.button();
